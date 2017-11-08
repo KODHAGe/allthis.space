5b9af5b2babcf83e094d7cb38249666a57ec5453
Wolf <wolf.wikgren@gmail.com>
2017-11-09 00:07:41 +0200

    Basic file upload support, realtime change propagation

--- src/components/Item.js
+++ src/components/Item.js
@@ -8,38 +8,70 @@ import firestore from 'firebase/firestore';
 class Item extends Component {
   constructor(props) {
     super(props);
-    this.db = firebase.firestore();    
-    this.state = {'id': this.props.id, 'name': this.props.name, 'x': this.props.positionX, 'y': this.props.positionY }
+    this.state = {'id': this.props.id, 'name': this.props.name, 'content': this.props.content, 'document': this.props.document, 'x': this.props.x, 'y': this.props.y, 'timestamp': this.props.timestamp ? this.props.timestamp.toString() : new Date().toString() };
+    console.log(this.state.document);
+    this.dbObj = firebase.firestore().collection('stage').doc(this.state.id);    
+  }
+  componentWillReceiveProps(newprops) {
+    this.setState({'id': newprops.id, 'name': newprops.name, 'content': newprops.content, 'x': newprops.x, 'y': newprops.y, 'timestamp': newprops.timestamp ? newprops.timestamp.toString() : new Date().toString() })
   }
   edit(event) {
     event.preventDefault();
     event.stopPropagation();
   }
-  save(event) {
-    let content = event.target.textContent;
-    let clicked = this.db.collection('stage').doc(this.state.id);
-    return clicked.update({
-      name: content
+  save(data, content) {
+    this.setState({content: content});
+    this.dbObj.update({
+      [data]: content,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp()
     })
     .then(function() {
       console.log("Document successfully updated!");
     })
     .catch(function(error) {
-        // The document probably doesn't exist.
         console.error("Error updating document: ", error);
+    });
+  }
+  delete(event) {
+    this.dbObj.delete()
+    .then(function() { 
+      console.log("Document deleted"); 
+    })
+    .catch(function (error) { 
+      console.log("Error: ", error);
+    });
+  }
+  dragstart(event) {
+  }
+  drag(event) {
+    //this.setState({x: event.clientX - 30, y: event.clientY - 160});    
+  }
+  dragend(event) {
+    this.setState({x: event.clientX - 30, y: event.clientY - 160});
+    this.dbObj.update({
+      position: [event.clientX - 30, event.clientY - 80],
+      timestamp: firebase.firestore.FieldValue.serverTimestamp()
+    })
+    .then(function() {
+      console.log("Document position successfully updated!");
+    })
+    .catch(function(error) {
+      console.error("Error updating document: ", error);
     });
   }
   render() {
     const style = {
-      top: this.props.positionY,
-      left: this.props.positionX
+      top: this.state.y,
+      left: this.state.x
     }
     return (
-      <div id={this.state.id} className='Item' style={style} onClick={this.edit}>
-        <h3 contentEditable='true' onInput={this.save.bind(this)}>{this.state.name}</h3>
-        <p contentEditable='true' onChange={this.save.bind(this)}>{this.state.id}</p>
-        <p contentEditable='true' onChange={this.save.bind(this)}>{this.state.x}</p>
-        <p contentEditable='true' onChange={this.save.bind(this)}>{this.state.y}</p>
+      <div id={this.state.id} className='Item' style={style} onClick={this.edit} onDragStart={this.dragstart} onDrag={(event) => this.drag(event)} onDragEnd={this.dragend.bind(this)} draggable="true">
+        <div className='id'>{this.state.id}</div>
+        <div className='timestamp'>{this.state.timestamp}</div>
+        <div className='Item-closebutton' onClick={this.delete.bind(this)}>×</div>
+        <h3 className='Item-name' contentEditable='true' onInput={(event) => this.save('name', event.target.textContent)} suppressContentEditableWarning>{this.state.name}</h3>
+        <img className='Item-image' src={this.state.document} />
+        <textarea className='Item-textarea' contentEditable='true' onChange={(event) => this.save('content', event.target.value)} value={this.state.content} suppressContentEditableWarning></textarea>
       </div>
     );
   }
--- src/components/Stage.js
+++ src/components/Stage.js
@@ -11,48 +11,97 @@ class Stage extends Component {
   constructor(props) {
     super(props);
     this.db = firebase.firestore();
+    this.storage = firebase.storage();
     this.state = { items: [] };
   }
   componentDidMount() {
-    let items = this.state.items;  
-    this.db.collection('stage').get().then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
+    console.log('didmount');
+    this.db.collection('stage').onSnapshot((snapshot) => {
+      console.log('Received update');
+      let items = [];      
+      snapshot.forEach((doc) => {
         let data = doc.data();
         data.key = doc.id;
         items.push(data);
       });
-      this.setState(items);      
+      this.setState({items: items});
+      console.log(this.state);
     });
   }
   placeLink(event) {
-    var clickCoordinates = [event.clientX - 30, event.clientY - 80];
     this.db.collection('stage').add({
-      name: 'cool',
-      storageUrl: 'cooler',
-      position: clickCoordinates
+      name: 'New note',
+      content: 'Write down ur luvly thoughts',
+      storageUrl: null,
+      position: [event.clientX - 30, event.clientY - 80],
+      timestamp: firebase.firestore.FieldValue.serverTimestamp()
     }).then((docRef) => {
       console.log("Document written with ID: ", docRef.id);
-      let items = this.state.items;
-      items.push({'key:': docRef, 'name':'New note', 'storageUrl': null, 'position': clickCoordinates});
-      this.setState(items);
-      console.log(this.state);
     })
     .catch(function(error) {
       console.error("Error adding document: ", error);
     });
   }
+  drag(event) {
+    event.preventDefault();
+    console.log(event)
+  }
+  dragover(event) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+  dragend(event) {
+    event.preventDefault();  
+    console.log('dragend');  
+    console.log(event)
+  }
+  drop(event) {
+    event.preventDefault();
+    console.log(this);
+    let file = event.dataTransfer.files[0];    
+    if(file && file.type.indexOf('image') == 0){
+      let storageRef = this.storage.ref();
+      let position = [event.clientX - 30, event.clientY - 80];
+      let itemRef = storageRef.child(file.name);
+      let reader = new FileReader();
+      reader.onload = (e) => {
+        itemRef.put(file).then((snapshot) => {
+          console.log('Upload successful')
+          itemRef.getDownloadURL().then((url) => {
+            this.db.collection('stage').add({
+              name: file.name,
+              content: 'Write down ur luvly thoughts',
+              storageUrl: url,
+              position: position,
+              timestamp: firebase.firestore.FieldValue.serverTimestamp()
+            }).then((docRef) => {
+              console.log("Document written with ID: ", docRef.id);
+            })
+            .catch(function(error) {
+              console.error("Error adding document: ", error);
+            });
+          })
+        })
+      }
+      reader.readAsDataURL(file);
+    }
+  }
   render() {
+    console.log('render fired');
     const items = this.state.items.map((item) => (
       <Item
         key={item.key}
         id={item.key}
+        document={item.storageUrl}
+        timestamp={item.timestamp}
         name={item.name}
-        positionX={item.position[0]}
-        positionY={item.position[1]}
+        content={item.content}
+        x={item.position[0]}
+        y={item.position[1]}
       />
     ));
     return (
-      <div className="Stage" onClick={this.placeLink.bind(this)}>
+      <div className="Stage" onClick={this.placeLink.bind(this)} onDrop={(event) => this.drop(event)} onDrag={(event) => this.drag(event)} onDragOver={(event) => this.dragover(event)}onDragEnd={(event) => this.dragend(event)}>
         {items}
       </div>
     );
